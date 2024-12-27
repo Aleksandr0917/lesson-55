b93c1d5171b04a5f377495264f70673722da3aad
Aleksandr0917 <180825344+Aleksandr0917@users.noreply.github.com>
2024-12-27 13:23:18 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,31 @@
+import time
+import multiprocessing
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:  # Если строка пустая, выходим из цикла
+                break
+            all_data.append(line.strip())  # Добавляем строку в список
+    # Возвращаем список all_data, но не выводим его в консоль
+    return all_data
+
+if __name__ == '__main__':
+    # Список названий файлов
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_duration = time.time() - start_time
+    print(f"{linear_duration:.6f} (линейный)")
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    multiprocessing_duration = time.time() - start_time
+    print(f"{multiprocessing_duration:.6f} (многопроцессный)")
